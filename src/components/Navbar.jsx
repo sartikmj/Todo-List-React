66a5a6dc680209a09e7487741936914f93772a75
sartikmj <sartikmj@gmail.com>
2025-10-02 01:09:19 +0530

    changed navbar name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <span className='font-bold text-xl mx-9'>iTask</span>
       </div>
         <ul className="flex gap-8 mx-9">
-          <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+          <li className='cursor-pointer hover:font-bold transition-all'>Made By Sartik !</li>
           <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
         </ul>
     </nav>
